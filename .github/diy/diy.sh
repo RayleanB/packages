139fdf8039f180beded92b2cac76960b799a994f
RayleanB <lein133@yahoo.com>
2025-04-24 23:07:25 +0800

    Create diy.sh

---
+++ .github/diy/diy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 用户自定义配置区（修改以下变量）
+SOURCE_REPO="https://github.com/kenzok8/small-package.git"  # 源仓库地址
+TARGET_DIR="my-packages"                                    # 目标目录名称
+CLONE_FOLDERS=""
+
+# 稀疏克隆函数
+function git_sparse_clone() {
+  branch="$1" rurl="$2" localdir="$3" && shift 3
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+  cd $localdir
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  mv -n $@ ../
+  cd ..
+  rm -rf $localdir
+}
+
+# 主克隆流程
+rm -rf $TARGET_DIR && mkdir $TARGET_DIR
+git_sparse_clone main "$SOURCE_REPO" "$TARGET_DIR" $CLONE_FOLDERS
+
+# 清理.git文件
+find $TARGET_DIR -name ".git*" -exec rm -rf {} \;
+
+# 自动提交
+git add .
+git commit -m "Sync: $(date +'%Y-%m-%d %H:%M:%S')"
+git push
